82ad4b7cd2e4bf96d66303f779fb1d0d928c0d99
Tan Wei Wen <weiwentan@shimmersensing.com>
2022-07-19 16:42:05 +0800

    minor

--- src/index.ts
+++ src/index.ts
@@ -348,19 +348,20 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   writeButton = document.getElementById('write') as HTMLButtonElement;
   writeButton.addEventListener('click', () => {
-    bytesToWriteInput = document.getElementById('bytesToWrite') as HTMLInputElement
-    var hexString = bytesToWriteInput.value;
+    bytesToWriteInput =
+    document.getElementById('bytesToWrite') as HTMLInputElement;
+    const hexString = bytesToWriteInput.value;
     if (hexString.length % 2 !== 0) {
-        throw "Must have an even number of hex digits to convert to bytes";
+      console.warn('Must have an even number of hex digits');
     }
-    var numBytes = hexString.length / 2;
-    var byteArray = new Uint8Array(numBytes);
-    for (var i=0; i<numBytes; i++) {
-        byteArray[i] = parseInt(hexString.substr(i*2, 2), 16);
+    const numBytes = hexString.length / 2;
+    const byteArray = new Uint8Array(numBytes);
+    for (let i=0; i<numBytes; i++) {
+      byteArray[i] = parseInt(hexString.substr(i*2, 2), 16);
     }
     if (port?.writable == null) {
-        console.warn(`unable to find writable port`);
-        return;
+      console.warn(`unable to find writable port`);
+      return;
     }
     const writer = port.writable.getWriter();
     writer.write(byteArray);
